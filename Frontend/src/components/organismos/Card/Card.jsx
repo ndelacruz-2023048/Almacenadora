0dc938e8e7ece9adeadad02330f44f57a32c7565
hcordero-2023253 <hcordero-2023253@kinal.edu.gt>
2025-04-22 22:28:42 -0600

    Se sube el Card y  Clientes

--- Frontend/src/components/organismos/Card/Card.jsx
+++ Frontend/src/components/organismos/Card/Card.jsx
@@ -1,16 +1,122 @@
 import React from 'react'
 import styled from 'styled-components'
+import photoProfile from '../../../assets/photoProfile.avif'
+import { CiMail } from "react-icons/ci";
+import { FiPhone } from "react-icons/fi";
+import { CiStar } from "react-icons/ci";
+import { GrFormNext } from "react-icons/gr";
+import { CiDiscount1 } from "react-icons/ci";
 
 export const Card = () => {
   return (
     <Container>
-        Card
+      <PartUp>
+        <Image src={photoProfile}/>
+        <NamePerson>John Doe</NamePerson>
+        <Position>Software Engineer</Position>
+      </PartUp>
+      <Line />
+      <PartDown>
+        <Contact><CiMail className='iconDown'/>john.doe@example.com</Contact>
+        <Contact><FiPhone  className='iconDown'/>john.doe@example.com</Contact>
+        <Links><CiStar className='iconDown'/>Reviews<GrFormNext /></Links>
+        <Links><CiDiscount1 className='iconDown'/>Sale<GrFormNext /></Links>
+      </PartDown>
     </Container>
   )
 }
 
 const Container = styled.div`
-    height: 180px;
-    width: 30%;
-    background-color: #849eb4;
+  height: 230px;
+  width: 30%;
+  background-color: #849eb4;
+`
+const PartUp = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`
+const PartDown = styled.div`
+  padding: 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  display: flex;
+`
+
+const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1.5px solid #28364b;
+  margin-left: 10px;
+  margin-top: 5px
+`
+const NamePerson = styled.span`
+  font-size: 16px;
+  color: #ffffff;
+  font-weight: bold;
+  text-shadow: 1px 1px 2px #000000;
+  text-align: left;
+  margin-left: 10px;
+  margin-top: -25px
+`
+
+const Position = styled.span`
+  font-size: 14px;
+  color: #ffffff;
+  margin-left: 10px;
+  margin-top: 5px;
+  font-style: italic;
+  margin-bottom: 5px;
+  line-height: 1.2;
+  margin-top: 20px;
+  margin-left: -70px;
+`
+const Line = styled.div`
+  height: 1px;
+  background-color: #000000;
+`
+const Contact = styled.p` 
+  font-size: 15px; 
+  color: #ffffff; 
+  margin-top: 5px;
+  margin-bottom: 5px;
+  line-height: 1.5;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .iconDown{
+    font-size: 20px;
+    color: #ffffff;
+    margin: 0 10px;
+  }
+`
+
+const Links = styled.p`
+  font-size: 15px; 
+  color: #ffffff; 
+  margin-top: 5px;
+  margin-bottom: 5px;
+  line-height: 1.5;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background-color: #8ab1f0;
+    transform: scale(1.1);
+    transition: background-color 0.3s, transform 0.3s;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+  
+  .iconDown{
+    font-size: 20px;
+    color: #ffffff;
+    margin: 0 10px;
+  }
 `
--- Frontend/src/components/templates/ClientTemplate.jsx
+++ Frontend/src/components/templates/ClientTemplate.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { CiGrid41 } from "react-icons/ci";
 import { Card } from '../organismos/Card/Card'
+import { FiMenu } from "react-icons/fi";
+import { GrAdd } from "react-icons/gr";
+
 
 export const ClientTemplate = () => {
   return (
     <Container>
+      <Line/>
       <Section1>
-        Cliente--Hector Cordero
+        <Header>All Client (5.5k)</Header>
+        <IconsContainer>
+            <CiGrid41  className='iconHeader'/>
+            <FiMenu className='iconHeader'/>
+        </IconsContainer>
+        <Add><GrAdd className='iconHeader'/> Add New</Add>
       </Section1>
+
       <Section2>
         <ContainerCard>
           <Card/>
@@ -33,8 +44,9 @@ const Container = styled.div`
 `
 
 const Section1 = styled.div`
-  background-color: red;
-  height: 12%;
+  background-color: transparent;
+  height: 10%;
+  display: flex
   `
 
 const Section2 = styled.div`
@@ -54,4 +66,50 @@ const ContainerCard = styled.div`
   margin: auto;
   background-color: aliceblue;
   overflow-y: scroll;
+`
+const Header = styled.div`
+  display: flex;
+  text-align: center;
+  font-size: 25px;
+  font-weight: bold;
+  width: 15%;
+`
+
+const IconsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    height: 60%;
+    width: 7%;
+    background-color : #dedada;
+    margin-left: 70%;
+    margin-right: 1%;
+    border-radius: 15px;
+    .iconHeader{
+        font-size: 30px;
+        margin-left: 10px;
+        color: #3636d3;
+    }
+`
+
+const Line = styled.div`
+  height: 1px;
+  background-color: #c8c8c8;
+`
+
+const Add = styled.button`
+  background-color: #5042cb;
+  color: white;
+  border: none;  
+  border-radius: 15px;
+  padding: 10px 20px;
+  cursor: pointer;
+  align-items: center;
+  display: flex;
+  width: 10%;
+  height: 60%;
+  .iconHeader{
+        align-items: center;
+        color: #ffffff;
+        margin: 7px
+    }
 `
