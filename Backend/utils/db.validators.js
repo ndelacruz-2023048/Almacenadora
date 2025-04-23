8918a136f85bd023123e10e6413a965c059661db
ccolorado-2023223 <ccolorado-2023223@kinal.edu.gt>
2025-04-23 00:25:38 -0600

    Se agrgan configuraciones y middlewares basicos en el backend

---
+++ Backend/utils/db.validators.js
@@ -0,0 +1,19 @@
+//Validaciones en caso de que existan usuario, descomentar o modificar en caso de ser necesario
+
+
+//Importar modelo User import User from '../src/user/user.model.js'
+/*export const existUsername = async(username, user)=>{
+    const alreadyUsername = await User.findOne({username})
+    if(alreadyUsername && alreadyUsername._id !=user._id){
+        console.error(`Username ${username} is already taken`)
+        throw new Error(`Username ${username} is already taken`)
+    }
+}
+
+export const existEmail = async(email, conpany)=>{
+    const alreadyEmail = await Company.findOne({email})
+    if(alreadyEmail && alreadyEmail._id != conpany._id){
+        console.error(`Email ${email} is already taken`)
+        throw new Error(`Email ${email} is already taken`)
+    }
+}*/
