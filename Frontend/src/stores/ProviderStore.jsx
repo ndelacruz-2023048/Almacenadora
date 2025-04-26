ab0d0fd82cff0eeaf0cdf6f843156190c7c4270a
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-04-26 13:26:01 -0600

    Se quito el store de ProductCategory

--- Frontend/src/stores/ProviderStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useProviderStore = create((set,get)=>({
-    isFormProviderOpen:false,
-    setIsFormProviderOpen:()=>{
-        const {isFormProviderOpen} = get()
-        set({isFormProviderOpen:isFormProviderOpen ? false:true})
-    }
-}))
