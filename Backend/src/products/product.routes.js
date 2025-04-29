aedc5f24f4ffedf26ddf3067debb7d6537cd95cd
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-04-29 06:27:05 -0600

    Se agrega la funcion de validar que no haya productos con el mismo nombre

--- Backend/src/products/product.routes.js
+++ Backend/src/products/product.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { 
     saveProduct,
+    getProductByName
 } from "./product.controller.js";
 
 const product = Router()
 
+product.get('/product/search', getProductByName)
 product.post('/product', saveProduct)
 
 export default product
--- Frontend/src/stores/ProductStore.jsx
+++ Frontend/src/stores/ProductStore.jsx
@@ -1,4 +1,5 @@
 import {create} from 'zustand'
+import axios from 'axios'
 export const useProductStore = create((set,get)=>({
     isFormOpen:false,
     setIsFormOpen:()=>{
@@ -33,5 +34,17 @@ export const useProductStore = create((set,get)=>({
     setIsListOptionsOpen:()=>{
         const {isListOptionsOpen} = get()
         set({isListOptionsOpen:isListOptionsOpen?false:true})
+    },
+    isSameProduct:[],
+    fetchProductByName:async(p)=>{
+        const paramsProduct = {
+            productName:p
+        }
+        const products = await axios.get('http://localhost:2900/v1/api/product/search',{params:paramsProduct})
+        const dataJSON = await products.data
+        set(()=>({isSameProduct:dataJSON}))
+        return{
+            dataJSON
+        }
     }
 }))
