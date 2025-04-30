7f915975c9cc729b7f4ec563adba5e00c4c3065e
ihernandez-2023019 <ignacio.hernandez@example.com>
2025-04-30 00:22:56 -0600

    Se agrega las funcionalidades para agregar a la base de datos

--- Backend/src/productCategory/productCategory.controller.js
+++ Backend/src/productCategory/productCategory.controller.js
@@ -1,10 +1,23 @@
-import ProductCategoryModel from "./productCategory.model.js"
+import ProductCategory from "./productCategory.model.js"
 export const getProductsCategory = async (req, res) => {
     try {
-        let clients = await ProductCategoryModel.find()
-        return res.status(200).send({message: 'Clients retrieved successfully', clients})
+        let categories = await ProductCategory.find()
+        return res.status(200).send({message: 'Categories retrieved successfully', categories})
     } catch (error) {
         console.error(error)
-        return res.status(500).send({message: 'Error retrieving clients', error})
+        return res.status(500).send({message: 'Error retrieving categories', error})
+    }
+}
+
+export const createProductsCategory = async (req, res) => {
+    try {
+        let data = req.body
+        console.log(data.productCategory);
+        let categories = new ProductCategory(data)
+        await categories.save()
+        return res.status(200).send({message: 'Created categorie succesfully', categories})
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({message: 'Error retrieving categories', error})
     }
 }
--- Backend/src/productCategory/productCategory.routes.js
+++ Backend/src/productCategory/productCategory.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { 
     getProductsCategory,
+    createProductsCategory
 } from "./productCategory.controller.js";
 
 const productCategory = Router()
 
 productCategory.get('/productCategory', getProductsCategory)
+productCategory.post('/productCategoryCreated', createProductsCategory)
 
 export default productCategory
