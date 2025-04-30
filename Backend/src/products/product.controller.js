36e34ce353d61c58adba99348c4e4bcb3e5e7464
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-04-29 22:48:46 -0600

    Se quita una ruta del product routes

--- Backend/src/products/product.controller.js
+++ Backend/src/products/product.controller.js
@@ -39,14 +39,3 @@ export const getProducts = async(req,res)=>{
         return res.status(500).send({message: 'Error finding client', error})
     }
 }
-
-// Obtener todos los productos
-export const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find()
-        return res.status(200).send(products)
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({message: 'Error fetching products', error})
-    }
-}
--- Backend/src/products/product.routes.js
+++ Backend/src/products/product.routes.js
@@ -4,14 +4,12 @@ import {
     saveProduct,
     getProductByName,
     getProducts,
-    getAllProducts
 } from "./product.controller.js";
 
 const product = Router()
 
 product.get('/product/search', getProductByName)
 product.post('/product', saveProduct)
-product.get('/product', getAllProducts)
 product.get('/product',getProducts)
 
 export default product
