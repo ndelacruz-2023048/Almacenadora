6b56599fcec384bc56139473d62ebae39337346b
hcordero-2023253 <hcordero-2023253@kinal.edu.gt>
2025-05-01 14:44:13 -0600

    Se suben funciones de invetariado

--- Backend/src/products/product.routes.js
+++ Backend/src/products/product.routes.js
@@ -4,14 +4,22 @@ import {
     saveProduct,
     getProductByName,
     getProducts,
-    updateProductStock
+    updateProductStock,
+    getInventoryReportWithPrice,
+    getInventoryMovementsWithPrice,
+    addProductStock,
+    subtractProductStock
 } from "./product.controller.js";
 
 const product = Router()
 
-product.get('/product/search', getProductByName)
 product.post('/product', saveProduct)
+product.get('/product/search', getProductByName)
 product.get('/product',getProducts)
+product.get('/product/inventoryReportWithPrice', getInventoryReportWithPrice);
+product.get('/product/inventoryMovementsWithPrice', getInventoryMovementsWithPrice);
 product.put('/product/updateStock/:id', updateProductStock)
+product.put('/product/addStock/:id', addProductStock);
+product.put('/product/subtractStock/:id', subtractProductStock);
 
 export default product
